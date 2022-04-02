76cf0c945d4ba41d111817cb659891cbc4740247
Paulo Roberto Ferreira Maciel <64757024+Paulo-RFM@users.noreply.github.com>
2022-04-01 22:25:17 -0300

    correção de warnnings
    funções declaradas no arquivo errado.

--- main.c
+++ main.c
@@ -2,49 +2,6 @@
 #include <stdio.h>
 #include "menu.c"
 #include "jogo.c"
-#include <ctype.h> //para ultilizar o isdigit()
-
-int conversao(char c){
-    if(c == '0'){
-        return 0;
-    }
-    if(c == '1'){
-        return 1;
-    }
-    if(c == '2'){
-        return 2;
-    }
-    if(c == '3'){
-        return 3;
-    }
-    if(c == '4'){
-        return 4;
-    }
-    if(c == '5'){
-        return 5;
-    }else{
-        return 0;
-    }
-}
-
-int atribuiValor(char c){
-    return conversao(c);
-}
-
-void scanff(int* op){
-    int aux = *op;
-    char c;
-    int result = -1;
-    do{
-        c = getchar();
-        result = isdigit(c); // se retornar 0 é caracter nao numeral, se retornar outro numero é caracter numero
-        if(result != 0){//se for numero
-            aux = atribuiValor(c);
-            c = '0';
-        }
-    }while(c != '0');
-    *op = aux;
-}
 
 int main(void){
     int opcao;
